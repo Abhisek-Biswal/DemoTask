7c1d0be0ee72074d65756ec50ff627781aa10c44
yudizsolutionsltd <abhisek.biswal@yudiz.com>
2023-06-01 15:35:02 +0530

    Retrofit

--- app/src/main/java/com/example/myapplication/ProductAdapter.kt
+++ app/src/main/java/com/example/myapplication/ProductAdapter.kt
@@ -1,10 +1,40 @@
 package com.example.myapplication
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 
-class ProductAdapter: RecyclerView.Adapter<ProductAdapter.ViewHolder> {
+class ProductAdapter( val productList :  MutableList<Product>) : RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
 
-    class ViewHolder: RecyclerView.ViewHolder{
-        ViewHolder(view itemView)
+    class ProductViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+
+
+         val productImg =  itemView.findViewById<ImageView>(R.id.imageView)
+
+         val productTitle = itemView.findViewById<TextView>(R.id.textView)
+         val productPrice = itemView.findViewById<TextView>(R.id.textView3)
+         val productCategory = itemView.findViewById<TextView>(R.id.textView2)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view =    layoutInflater.inflate(R.layout.item_of_products, parent, false)
+        return ProductViewHolder(view)
+    }
+    override fun getItemCount() = productList.size
+
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+        val position = productList[position]
+        holder.productTitle.text = position.title
+        holder.productPrice.text = "$"+position.price.toString()
+        holder.productCategory.text = position.category
+
+
+        Picasso.get().load(position.thumbnail).into(holder.productImg)
     }
 }
--- app/src/main/java/com/example/myapplication/Retrofrit_Demo.kt
+++ app/src/main/java/com/example/myapplication/Retrofrit_Demo.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -15,12 +16,14 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 
 class Retrofrit_Demo : AppCompatActivity() {
+    lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_retrofrit_demo)
 
 
 
+        recyclerView=findViewById(R.id.recycler_view)
 
         println(getData())
     }
@@ -47,7 +50,11 @@ class Retrofrit_Demo : AppCompatActivity() {
         // on below line we are making a call.
         call!!.enqueue(object : Callback<ProductList?> {
             override fun onResponse(call: Call<ProductList?>, response: Response<ProductList?>) {
-                response.body().toString()
+                val retrofit2= response.body()?.let {ProductAdapter(it.products)}
+                recyclerView.adapter = retrofit2
+
+
+
             }
 
 
