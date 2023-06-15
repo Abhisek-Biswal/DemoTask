5c18474bde9dc657eaccfa43736a6d80b5e71505
YudizAndroidAbhisek <abhisek.biswal@yudiz.com>
2023-06-15 09:58:12 +0530

    Notification(continue)

--- app/src/main/java/com/example/myapplication/NotificationAct.kt
+++ app/src/main/java/com/example/myapplication/NotificationAct.kt
@@ -1,11 +1,12 @@
 package com.example.myapplication
 
-import android.annotation.SuppressLint
-import android.app.*
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.graphics.BitmapFactory
-import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.widget.Button
@@ -60,26 +61,29 @@ class NotificationAct : AppCompatActivity() {
             // If Min. API level of the phone is 26, then notification could be
             // made asthetic
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                notifChannel = NotificationChannel(channelID,description,
-                    NotificationManager.IMPORTANCE_HIGH)
+                notifChannel = NotificationChannel(
+                    channelID, description,
+                    NotificationManager.IMPORTANCE_HIGH
+                )
                 notifChannel.enableVibration(true)
 
                 notifManager.createNotificationChannel(notifChannel)
 
-                notifBuilder = Notification.Builder(this,channelID)
+                notifBuilder = Notification.Builder(this, channelID)
                     .setContentTitle("Title")
                     .setContentText("Text")
                     .setSmallIcon(R.mipmap.ic_launcher_round)
 
                     // Command to Insert a message in the Notification
-                    .setStyle(Notification.BigTextStyle()// <-- Look here
-                        .bigText(message))// <---- Look here
-
+                    .setStyle(
+                        Notification.BigTextStyle()// <-- Look here
+                            .bigText(message)
+                    )// <---- Look here
 
 
             }
             // Else the Android device would give out default UI attributes
-            else{
+            else {
                 notifBuilder = Notification.Builder(this)
                     .setContentTitle("Title")
                     .setContentText("Text")
@@ -88,11 +92,10 @@ class NotificationAct : AppCompatActivity() {
 
             // Everything is done now and the Manager is to be notified about
             // the Builder which built a Notification for the application
-            notifManager.notify(1234,notifBuilder.build())
+            notifManager.notify(1234, notifBuilder.build())
         }
 
         btn2.setOnClickListener {
-
 
 
 //            val textMessage = "Click On the Message"
@@ -102,7 +105,8 @@ class NotificationAct : AppCompatActivity() {
 
             val intent = Intent(this, AfterNotificationAct::class.java)
 
-            val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+            val pendingIntent =
+                PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
 
             // RemoteViews are used to use the content of
             // some different layout apart from the current activity layout
@@ -110,24 +114,25 @@ class NotificationAct : AppCompatActivity() {
 
             // checking if android version is greater than oreo(API 26) or not
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                notifChannel = NotificationChannel(channel_id, description1, NotificationManager.IMPORTANCE_HIGH)
+                notifChannel = NotificationChannel(
+                    channel_id,
+                    description1,
+                    NotificationManager.IMPORTANCE_HIGH
+                )
                 notifChannel.enableVibration(false)
                 notifManager.createNotificationChannel(notifChannel)
 
                 notifBuilder = Notification.Builder(this, channel_id)
-//                    .setContent(contentView)
-//                    .setSmallIcon(R.drawable.ic_launcher_background)
-//                    .setContentTitle("Title")
-//                    .setContentText("Click on the Message")
                     .setContentTitle("Title")
                     .setContentText("Click on this Message")
                     .setSmallIcon(R.mipmap.ic_launcher_round)
-                    .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.ic_launcher_background))
+                    .setLargeIcon(
+                        BitmapFactory.decodeResource(
+                            this.resources,
+                            R.drawable.ic_launcher_background
+                        )
+                    )
                     .setContentIntent(pendingIntent)
-
-
-
-
 
 
             } else {
@@ -136,13 +141,43 @@ class NotificationAct : AppCompatActivity() {
                     .setContentTitle("Title")
                     .setContentText("Click on this Message")
                     .setSmallIcon(R.mipmap.ic_launcher_round)
-                    .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.ic_launcher_background))
+                    .setLargeIcon(
+                        BitmapFactory.decodeResource(
+                            this.resources,
+                            R.drawable.ic_launcher_background
+                        )
+                    )
                     .setContentIntent(pendingIntent)
             }
             notifManager.notify(1234, notifBuilder.build())
         }
         btn3.setOnClickListener {
 
+
+
+                val notificationLayout = RemoteViews(packageName,
+                    R.layout.custom_notification_act
+                )
+                notifBuilder = Notification.Builder(this, "channel_id2")
+                    .setContentTitle("Result")
+                    .setSmallIcon(R.drawable.ic_image)
+                    .setStyle(Notification.DecoratedCustomViewStyle())
+                    .setCustomBigContentView(notificationLayout)
+                    .setPriority(Notification.PRIORITY_DEFAULT)
+                notifManager.notify(1234, notifBuilder.build())
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//                    val name = "Notification Channel3"
+//                    val descriptionText = "Notification Description"
+//                    val importance = NotificationManager.IMPORTANCE_DEFAULT
+                      notifChannel = NotificationChannel(channel_id2, description2, NotificationManager.IMPORTANCE_DEFAULT).apply {
+                    }
+                    notifManager.createNotificationChannel(notifChannel)
+                }
+
+
+
         }
+
     }
 }
